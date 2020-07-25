44ff996f57c7b0e33e5f8500123be563f2194945
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-07-25 16:36:37 -0700

    creating custom middleware

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
+
+
+
+app.use((req,res,next) => {
+  console.log('Hello from the middleware')
+  next();
+});
+
+app.use((req,res,next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 
 const tours = JSON.parse(
@@ -11,8 +22,10 @@ const tours = JSON.parse(
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
