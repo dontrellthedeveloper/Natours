54c08e3cf79e2d20fa2c689fd54d6f949f361db2
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-07-28 14:56:06 -0700

    refactored application using MVC

---
+++ models/tourModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const testTour = new Tour ({
+//   name: 'The Park Camper 2',
+//   price: 997
+// });
+
+// testTour.save().then(doc => {
+//   console.log(doc);
+// }).catch(err => {
+//   console.log('ERROR :', err)
+// });
--- server.js
+++ server.js
@@ -13,37 +13,6 @@ mongoose.connect(DB, {
 }).then(() => console.log('DB connection successful'));
 
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour ({
-  name: 'The Park Camper 2',
-  price: 997
-});
-
-testTour.save().then(doc => {
-  console.log(doc);
-}).catch(err => {
-  console.log('ERROR :', err)
-});
-
-console.log(process.env);
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
