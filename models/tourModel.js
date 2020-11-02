8b5171aa46bec0953a30ab22210b8119288666ab
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-02 12:43:13 -0800

    starting to model reviews using parent referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -112,7 +112,8 @@ const tourSchema = new mongoose.Schema({
 {
   toJSON: {virtuals: true},
   toObject: {virtuals: true}
-});
+}
+);
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
