2bef111bf94fa966927914dfb0ffa114214e1e71
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-05 08:11:21 -0800

    created a views folder and installed pug to dynamically render html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,7 +14,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1. Global Middleware
+// Serving static files
+app.use(express.static(path.join(`${__dirname}/public`)));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -52,8 +59,7 @@ app.use(hpp({
   ]
 }));
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
+
 
 // Test middleware
 app.use((req,res,next) => {
@@ -66,6 +72,10 @@ app.use((req,res,next) => {
 
 
 // 3. Routes
+app.get('/', (req,res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
