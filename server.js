0c318cf99d5f999f472177933dc21584da832dfb
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-10 15:43:11 -0800

    added heroku config for SIGTERM signal

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ process.on('unhandledRejection', err => {
   });
 });
 
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+   console.log('💥 Process terminated')
+  })
+});
 
 
 
