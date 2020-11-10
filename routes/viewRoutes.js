543c84cf9e4a2f0d2bc29a28ec235d1cadae7ef2
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-10 09:11:05 -0800

    created a new booking on checkout success

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
 
-router.get('/', authController.isLogginIn, viewController.getOverview);
+router.get('/', authController.isLogginIn, bookingController.createBookingCheckout, viewController.getOverview);
 router.get('/tour/:slug', authController.isLogginIn, viewController.getTour);
 router.get('/login', authController.isLogginIn, viewController.getLoginForm);
 router.get('/me',  authController.protect, viewController.getAccount);
