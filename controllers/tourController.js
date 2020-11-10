9c83f4f87b2594500b437aa4fa4f0f0664133234
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-10 04:18:46 -0800

    integrated stripe payment system into back-end

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,6 @@
-const Tour = require('./../models/tourModel');
 const multer = require('multer');
 const sharp = require('sharp');
+const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
